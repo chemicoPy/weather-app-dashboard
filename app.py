af1a7486f9225425c9d2c62cc2b1fe4813446b34
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-18 10:40:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -66,44 +66,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title('Weather App dashboard')
 st.subheader("Navigate to side bar to see full project info as well as options to choose from, to get started!")
 
-  
-from forex_python.converter import CurrencyRates
-from forex_python.converter import CurrencyCodes
 
-class CurrencyConverter:
-    def __init__(self, YOUR_APP_ID, symbols):
-            self.YOUR_APP_ID = "5b709615dfbf4532bb3296a5ea23c7c6"
-            self.symbols = symbols
-            self._symbols = ",".join([str(s) for s in symbols])
-
-            r = requests.get(
-                "https://openexchangerates.org/api/latest.json",
-                params = {
-                    "app_id" : self.YOUR_APP_ID,
-                    "symbols" : self._symbols,
-                    "show_alternatives": True
-                        }
-                )
-            self.rates_ = r.json()["rates"]
-            self.rates_["USD"] = 1
-
-    def convert(self, value, symbol_from, symbol_to):
-        try:
-            return value * 1/self.rates_.get(symbol_from) * self.rates_.get(symbol_to)
-        except TypeError:
-            print("Error")
-            return None
-
-YOUR_APP_ID = "5b709615dfbf4532bb3296a5ea23c7c6"
-simpleConverter = CurrencyConverter(YOUR_APP_ID, ["MATIC" , "XAU","BTC","ETH","DOGE", "GBP", 
-             "EUR", "NZD", "USD", "NPR", "BTC", "JPY","BGN","CZK","DKK","GBP","HUF","PLN","RON","SEK", 
-                                                  "CHF","ISK","NOK","TRY","AUD","BRL","CAD","CNY","HKD","IDR","ILS",
-                                                  "INR","KRW","MXN","MYR","PHP","SGD", "THB", "ZAR"])
-
-
-
-c = CurrencyRates()
-    
 from PIL import Image
 
 def add_logo(logo_path, width, height):
@@ -115,35 +78,36 @@ def add_logo(logo_path, width, height):
 my_logo = add_logo(logo_path="img/icon.png", width=60, height=70)
 st.sidebar.image(my_logo)
 
+API_KEY = "06a0159059dc1c945b62591f03bbf59d"
+BASE_URL = "http://api.openweathermap.org/data/2.5/forecast"
+  
   
     # ------ layout setting---------------------------
 st.sidebar.markdown(
             """
     ## Project Overview
-    Crypto Converter is an app that converts crypto coin price to a local currency price. You can check price on a visualization board as well when you select the option and hit on the button.
-    
+    Weather App dashboard is an app that...
     Get started already!""")    
 
   
-st.sidebar.markdown("## Select Crypto pair & Interval below") # add a title to the sidebar container
+st.sidebar.markdown("## Select Country & City from below options") # add a title to the sidebar container
 
 from_conv = st.sidebar.selectbox(
-            "Convert From",
+            "Country",
             ("GBP", 
 "EUR", "NZD", "USD", "NPR","JPY","BGN","CZK","DKK","GBP","HUF","PLN","RON","SEK", 
               "CHF","ISK","NOK","TRY","AUD","BRL","CAD","CNY","HKD","IDR","ILS",
                "INR","KRW","MXN","MYR","PHP","SGD", "THB", "ZAR"),)
 
 to_conv = st.sidebar.selectbox(
-            "Convert To",
+            "City",
             ("GBP", 
              "EUR", "NZD", "USD", "NPR", "JPY","BGN","CZK","DKK","GBP","HUF","PLN","RON","SEK", 
                                                   "CHF","ISK","NOK","TRY","AUD","BRL","CAD","CNY","HKD","IDR","ILS", "INR","KRW","MXN","MYR","PHP","SGD", "THB", "ZAR"),)
     
-price = st.sidebar.number_input("Enter price to convert")
 
-st.sidebar.write("Converted price = ", simpleConverter.convert(price, str(from_conv), str(to_conv)))
-#st.write("Converted price= ", (c.get_symbol(to_conv), simpleConverter.convert(price, from_conv, to_conv)) 
+#st.sidebar.write("Converted price = ", simpleConverter.convert(price, str(from_conv), str(to_conv)))
+
 
     # ---------------forex pair selection------------------
   
@@ -157,119 +121,37 @@ Tframe = st.sidebar.selectbox(
         '', ["Interval of interest", "1m","5m","15m","30m","1h","2h","4h","1d","1w","month"], index=0)
 
 if st.sidebar.button("Show Viz!"):
-  lim = 1000
-  bybit = ccxt.bybit()
+    # Set parameters for the API request
+  city = "Grande cache"
+  country = "CA"
+  units = "metric"
 
-  if instrument=="Select Forex Pair of interest" and Tframe=="Interval of interest":
-    st.write("Kindly navigate to sidebar to see more options...")
+# Make the API requestr
+  response = requests.get(BASE_URL, params={
+    "q": f"{city},{country}",
+    "units": units,
+    "appid": API_KEY
+})
 
-  else:
-    klines = bybit.fetch_ohlcv(instrument, timeframe=Tframe, limit= lim, since=None)
+# Parse the API response
+  data = response.json()
 
-    from datetime import datetime, timedelta
-    one_month_ago = datetime.now() - timedelta(days=30)
-    filtered_klines = [kline for kline in klines if datetime.fromtimestamp(kline[0]/1000) >= one_month_ago]
-    
-# convert the klines list to a DataFrame
-    df = pd.DataFrame(filtered_klines, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+# Extract the forecast data for each day
+  days = []
+  temps = []
+  for day in data["list"]:
+    days.append(day["dt_txt"])
+    temps.append(day["main"]["temp"])
 
-# convert the timestamp column to a datetime type
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+# Create a pandas DataFrame with the forecast data
+  df = pd.DataFrame({"Day": days, "Temperature (C)": temps})
 
-# set the timestamp column as the index
-    df.set_index('timestamp', inplace=True)
+# Add a chart showing the forecast temperature
+  st.line_chart(df)
 
+  st.write(fig)
 
-# calculate RSI
-    df['rsi'] = ta.rsi(df['close'])
-
-# calculate Stochastics
-    stoch_data = df.copy()
-
-#df['stoch_k'], df['stoch_d'] = ta.stoch(df['high'], df['low'], df['close'])
-#New calculation for Stochastic
-    stoch_data['high'] = stoch_data['high'].rolling(14).max()
-    stoch_data['low'] = stoch_data['low'].rolling(14).min()
-    stoch_data['%k'] = (stoch_data["close"] - stoch_data['low'])*100/(stoch_data['high'] - stoch_data['low'])
-    stoch_data['%d'] = stoch_data['%k'].rolling(3).mean()
-    df['stoch_data'] = stoch_data['%d']
-
-
-# Candlestick plot
-
-    data = [go.Candlestick(x=df.index,
-                       open=df.open,
-                       high=df.high,
-                       low=df.low,
-                       close=df.close)]
-
-    layout = go.Layout(title= f'{instrument} in {Tframe} Candlestick with Range Slider',
-                   xaxis={'rangeslider':{'visible':True}})
-    fig = go.Figure(data=data,layout=layout)
-    plt.show()
-    st.write(fig)
-
-    
-    import plotly.graph_objs as go
-
-# create a line chart using the close data
-    line = go.Scatter(
-        x=df.index,
-        y=df['close'],
-        name='Close',
-        line=dict(color='#17BECF')
-)
-
-# create the layout for the chart
-    layout = go.Layout(
-        title= f'{instrument} in {Tframe} Kline Data',
-    xaxis=dict(
-        title='Time',
-        rangeslider=dict(visible=True)
-    ),
-    yaxis=dict(
-        title='Price (USDT)'
-    )
-)
-
-# create the figure and plot the chart
-    fig = go.Figure(data=[line], layout=layout)
-    plt.show()
-    st.write(fig)
-
-    import plotly.express as px
-
-# create the line chart
-    fig = px.line(df, x=df.index, y=df['rsi'], title=f'{instrument} in {Tframe} Kline Data')
-
-# add the red and green lines
-    fig.add_shape(
-    type='line',
-    x0=df.index[0],
-    x1=df.index[-1],
-    y0=75,
-    y1=75,
-    yref='y',
-    line=dict(color='red', dash='dot')
-)
-        
-    fig.add_shape(
-    type='line',
-    x0=df.index[0],
-    x1=df.index[-1],
-    y0=25,
-    y1=25,
-    yref='y',
-    line=dict(color='green', dash='dot')
-)
-
-# set the y-axis range to include 0 and 100
-    fig.update_layout(yaxis=dict(range=[0, 100]))
-
-    plt.show()
-    st.write(fig)
-
-    st.write(df)
+  st.write(df)
     
 st.sidebar.markdown(
 
