d8652f2906483ab4492dd4f83b6a88a785c96404
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-18 15:04:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -130,12 +130,12 @@ if country_select !="Country":
   
 st.write("\n")  # add spacing   
 
-st.sidebar.markdown("## Visualization")    
-instrument = st.sidebar.selectbox(
-        '', ["Select Forex Pair of interest", "MATIC/USDT" , "XAU/USDT","BTC/USDT","ETH/USDT",
-             "DOGE/USDT", "BNB/USDT", "USD/USDT", "XRP/USDT", "SOL/USDT", "TRX/USDT", "LTC/USDT", "SHIB/USDT"], index=0)
-Tframe = st.sidebar.selectbox(
-        '', ["Interval of interest", "1m","5m","15m","30m","1h","2h","4h","1d","1w","month"], index=0)
+
+
+
+
+
+
 
 if st.sidebar.button("Show Viz!"):
     # Set parameters for the API request
