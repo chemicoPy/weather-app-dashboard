034dd81620cd66c3b13a3a0d0a0010ce8f83b23e
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-22 13:12:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -136,71 +136,124 @@ else:
     
   
 st.write("\n")  # add spacing   
-
-if st.sidebar.button("Go!"):
-  url = f"http://api.openweathermap.org/data/2.5/weather?q={city_select}&appid={API_KEY}"
-  response = requests.get(url)
-  res = response.json()
-  BASE_URL = "http://api.openweathermap.org/data/2.5/forecast"
-  units = "metric"
-
-# Make the API requestr
-  response_2 = requests.get(BASE_URL, params={
-    "q": f"{city_select},{country_select}",
-    "units": units,
-    "appid": API_KEY
-})
-
-# Parse the API response
-  data = response.json()
- 
-# Parse the API response
-  data_2 = response_2.json()
-
-# Extract the forecast data for each day
-  days = []
-  temps = []
-  pressure_list = []
-  humidity_list = []
-  description_list = []
-  temp_min_list = []
-  temp_max_list = []
-  temp_kf_list = []
   
-  for day in data_2["list"]:
-    days.append(day["dt_txt"])
-    temps.append(day["main"]["temp"])
-    pressure_list.append(day["main"]["pressure"])
-    humidity_list.append(day["main"]["humidity"])
-    temp_min_list.append(day["main"]["temp_min"])
-    temp_max_list.append(day["main"]["temp_max"])
-    temp_kf_list.append(day["main"]["temp_kf"])
-    description_list.append(day["weather"][0]["description"].capitalize())
-    
-# Create a pandas DataFrame with the forecast data
-  df = pd.DataFrame({"Day": days, "Temperature (C)": temps, "Pressure": pressure_list, "Humidity": humidity_list,"Insight": description_list, 
-                     "Temp (min)": temp_min_list, "Temp (max)": temp_max_list, "Temp (kf)": temp_kf_list})
+if(st.button("Go!")):
+    try:
+        lon=x["coord"]["lon"]
+        lat=x["coord"]["lat"]
+        ex="current,minutely,hourly"
+        url2=f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={ex}&appid={api}'
+        res=requests.get(url2)
+        y=res.json()
 
-# Add a chart showing the forecast temperature
-  st.line_chart(df)
-  
-  #temperature
-  temp = data_2["list"][0]["main"]["temp"]
-  st.write(temp,chr(176) + "C")
-  #pressure
-  pressure = res["main"]
-  st.write(pressure["pressure"]/100, "bar")
-  #Description
-  desc = res["weather"][0]["description"].capitalize()
-  st.write(desc)
+        maxtemp=[]
+        mintemp=[]
+        pres=[]
+        humd=[]
+        wspeed=[]
+        desc=[]
+        cloud=[]
+        rain=[]
+        dates=[]
+        sunrise=[]
+        sunset=[]
+        cel=273.15
+        
+        for item in y["daily"]:
+            
+            if unit=="Celsius":
+                maxtemp.append(round(item["temp"]["max"]-cel,2))
+                mintemp.append(round(item["temp"]["min"]-cel,2))
+            else:
+                maxtemp.append(round((((item["temp"]["max"]-cel)*1.8)+32),2))
+                mintemp.append(round((((item["temp"]["min"]-cel)*1.8)+32),2))
 
-  st.subheader("Suggestion/Insights")
-  #st.write()
-  
-  
-  st.write(df)
-  
-    
+            if wind_unit=="m/s":
+                wspeed.append(str(round(item["wind_speed"],1))+wind_unit)
+            else:
+                wspeed.append(str(round(item["wind_speed"]*3.6,1))+wind_unit)
+
+            pres.append(item["pressure"])
+            humd.append(str(item["humidity"])+' %')
+            
+            cloud.append(str(item["clouds"])+' %')
+            rain.append(str(int(item["pop"]*100))+'%')
+
+            desc.append(item["weather"][0]["description"].title())
+
+            d1=datetime.date.fromtimestamp(item["dt"])
+            dates.append(d1.strftime('%d %b'))
+            
+            sunrise.append( datetime.datetime.utcfromtimestamp(item["sunrise"]).strftime('%H:%M'))
+            sunset.append( datetime.datetime.utcfromtimestamp(item["sunset"]).strftime('%H:%M'))
+
+        def bargraph():
+            fig=go.Figure(data=
+                [
+                go.Bar(name="Maximum",x=dates,y=maxtemp,marker_color='crimson'),
+                go.Bar(name="Minimum",x=dates,y=mintemp,marker_color='navy')
+                ])
+            fig.update_layout(xaxis_title="Dates",yaxis_title="Temperature",barmode='group',margin=dict(l=70, r=10, t=80, b=80),font=dict(color="white"))
+            st.plotly_chart(fig)
+        
+        def linegraph():
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=dates, y=mintemp, name='Minimum '))
+            fig.add_trace(go.Scatter(x=dates, y=maxtemp, name='Maximimum ',marker_color='crimson'))
+            fig.update_layout(xaxis_title="Dates",yaxis_title="Temperature",font=dict(color="white"))
+            st.plotly_chart(fig)
+            
+        icon=x["weather"][0]["icon"]
+        current_weather=x["weather"][0]["description"].title()
+        
+        if unit=="Celsius":
+            temp=str(round(x["main"]["temp"]-cel,2))
+        else:
+            temp=str(round((((x["main"]["temp"]-cel)*1.8)+32),2))
+        
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("## Current Temperature ")
+        with col2:
+            st.image(f"http://openweathermap.org/img/wn/{icon}@2x.png",width=70)
+
+        
+        col1, col2= st.columns(2)
+        col1.metric("TEMPERATURE",temp+temp_unit)
+        col2.metric("WEATHER",current_weather)
+        st.subheader(" ")
+        
+        if graph=="Bar Graph":
+            bargraph()
+            
+        elif graph=="Line Graph":
+            linegraph()
+
+         
+        table1=go.Figure(data=[go.Table(header=dict(
+                  values = [
+                  '<b>DATES</b>',
+                  '<b>MAX TEMP<br>(in'+temp_unit+')</b>',
+                  '<b>MIN TEMP<br>(in'+temp_unit+')</b>',
+                  '<b>CHANCES OF RAIN</b>',
+                  '<b>CLOUD COVERAGE</b>',
+                  '<b>HUMIDITY</b>'],
+                  line_color='black', fill_color='royalblue',  font=dict(color='white', size=14),height=32),
+        cells=dict(values=[dates,maxtemp,mintemp,rain,cloud,humd],
+        line_color='black',fill_color=['paleturquoise',['palegreen', '#fdbe72']*7], font_size=14,height=32
+            ))])
+
+        table1.update_layout(margin=dict(l=10,r=10,b=10,t=10),height=328)
+        st.write(table1)
+        
+        table2=go.Figure(data=[go.Table(columnwidth=[1,2,1,1,1,1],header=dict(values=['<b>DATES</b>','<b>WEATHER CONDITION</b>','<b>WIND SPEED</b>','<b>PRESSURE<br>(in hPa)</b>','<b>SUNRISE<br>(in UTC)</b>','<b>SUNSET<br>(in UTC)</b>']
+                  ,line_color='black', fill_color='royalblue',  font=dict(color='white', size=14),height=36),
+        cells=dict(values=[dates,desc,wspeed,pres,sunrise,sunset],
+        line_color='black',fill_color=['paleturquoise',['palegreen', '#fdbe72']*7], font_size=14,height=36))])
+        
+        table2.update_layout(margin=dict(l=10,r=10,b=10,t=10),height=360)
+        st.write(table2)
+        
 st.sidebar.markdown(
 
     """
