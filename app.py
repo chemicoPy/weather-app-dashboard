59f4c89eb94aab9130338ffae31933c4fc2bdb08
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-22 14:14:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ st.markdown(
     """
     <style>
     .reportview-container {
-        background: url("https://github.com/chemicoPy/weather-app-dashboard/blob/main/img/bg_img.jpg")
+        background: url("https://www.pexels.com/photo/island-during-golden-hour-and-upcoming-storm-1118873/")
     }
   
     </style>
