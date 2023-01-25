77c7d48b5774a316e84d5fc423232352c52d855d
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-26 00:03:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,41 +35,9 @@ import datetime
   
 
 # Desiging & implementing changes to the standard streamlit UI/UX
-st.set_page_config(page_icon="img/page_icon.png")    #Logo
+st.set_page_config(page_icon="img/page_icon.png", initial_sidebar_state='expanded')    #Logo
 
 st.title("Weather App Dashboard 🌧️🌥️")
-
-st.markdown('''<style>.css-1egvi7u {margin-top: -4rem;}</style>''',
-    unsafe_allow_html=True)
-# Design change hyperlink href link color
-st.markdown('''<style>.css-znku1x a {color: #9d03fc;}</style>''',
-    unsafe_allow_html=True)  # darkmode
-st.markdown('''<style>.css-znku1x a {color: #9d03fc;}</style>''',
-    unsafe_allow_html=True)  # lightmode
-# Design change height of text input fields headers
-st.markdown('''<style>.css-qrbaxs {min-height: 0.0rem;}</style>''',
-    unsafe_allow_html=True)
-# Design change spinner color to primary color
-st.markdown('''<style>.stSpinner > div > div {border-top-color: #9d03fc;}</style>''',
-    unsafe_allow_html=True)
-# Design change min height of text input box
-st.markdown('''<style>.css-15tx938{min-height: 0.0rem;}</style>''',
-    unsafe_allow_html=True)
-
-
-
-# Design hide top header line
-hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
-st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
-# Design hide "made with streamlit" footer menu area
-hide_streamlit_footer = """<style>#MainMenu {visibility: hidden;}
-                        footer {visibility: hidden;}</style>"""
-st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
-
-# disable warnings
-st.set_option('deprecation.showPyplotGlobalUse', False)
-st.set_option('deprecation.showfileUploaderEncoding', False)
-
 
 st.subheader("Navigate to side bar to see full project info as well as options to choose from, to get started!")
 
